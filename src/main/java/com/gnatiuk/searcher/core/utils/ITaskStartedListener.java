6a406b15d5fcbda83f32ac31f3b20fa09006f6e1
segn <sergiy-gnatyuk@yandex.ru>
2015-06-05 17:17:21 +0300

    added task started listener

---
+++ src/main/java/com/gnatiuk/searcher/core/utils/ITaskStartedListener.java
@@ -0,0 +1,8 @@
+package com.gnatiuk.searcher.core.utils;
+
+/**
+ * Created by sgnatiuk on 6/5/15.
+ */
+public interface ITaskStartedListener {
+    void actionPerformed(TaskStartedEvent event);
+}
