4bee3dacacc413c3f3b2897d286a1ee650b23255
segn <sergiy-gnatyuk@yandex.ru>
2015-10-29 16:19:54 +0200

    inheritanvce FilterFileKeywordTest from FilterFileReaderTest

--- searcher-filters-lib/src/test/java/com/gnatiuk/filters/internal/FilterFileKeywordTest.java
+++ searcher-filters-lib/src/test/java/com/gnatiuk/filters/internal/FilterFileKeywordTest.java
@@ -1,15 +1,14 @@
 package com.gnatiuk.filters.internal;
 
-import com.gnatiuk.filters.AFilterTest;
 import com.gnatiuk.searcher.filters.IFilter;
 import com.gnatiuk.searcher.filters.internal.FilterFileKeyword;
 
 /**
  * Created by Батинчук on 28.10.2015.
  */
-public class FilterFileKeywordTest extends AFilterTest {
+public class FilterFileKeywordTest extends AFilterFileReaderTest {
     @Override
     protected IFilter getFilter() {
-        return new FilterFileKeyword();
+        return new FilterFileKeyword(keywords);
     }
 }
